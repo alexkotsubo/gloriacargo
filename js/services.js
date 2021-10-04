0ca1d84311654a7585ceae86b3b425f995b39bdc
alexkotsubo <66830322+alexkotsubo@users.noreply.github.com>
2021-10-04 15:27:31 +0300

    с

--- js/services.js
+++ js/services.js
@@ -2,56 +2,57 @@
 
 /* Growing Numbers */
 
-const animNumbers = () => {
-	const offset = elem => {
-		let rect = elem.getBoundingClientRect(),
-				scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-				scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-		return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
-	}
-
-	const featuresNumbersElems = document.querySelectorAll('.about__feature-title span');
-	const featuresNumbers = [];
-	const featuresNumbersNotChanged = [];
-	for (let i = 0, length = featuresNumbersElems.length; i < length; i++) {
-		const animStart = 4;
-		let animItemPoint = window.innerHeight - featuresNumbersElems[i].offsetHeight / animStart;
-		if (featuresNumbersElems[i].offsetHeight > window.innerHeight) animItemPoint = window.innerHeight - window.innerHeight / animStart;
-		if ((pageYOffset > offset(featuresNumbersElems[i]).top - animItemPoint) && pageYOffset < (offset(featuresNumbersElems[i]).top + featuresNumbersElems[i].offsetHeight)) {
-			for (let y = 0; y < featuresNumbersElems.length; y++) {
-				let value = featuresNumbersElems[y].innerHTML;
-				featuresNumbersNotChanged.push(value);
-				featuresNumbers.push(value.replace(/[\s.,\+]/g, ''));
-			}
-
-			const smooth = 70;
-			const seconds = 1000;
-			if (smooth >= 50 && smooth <= seconds) {
-				let value = 0;
-				const num = featuresNumbers[i];
-				const period = seconds / smooth;
-				const addNum = featuresNumbers[i] / period;
-				const increment = setInterval(() => {
-					if (value <= featuresNumbers[i]) {
-						const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-						featuresNumbersElems[i].innerHTML = numberWithCommas(Math.trunc(value)) + '+';
-						Math.trunc(value);
-					} else {
-						featuresNumbersElems[i].innerHTML = featuresNumbersNotChanged[i];
-						clearInterval(increment);
-					}
-					value += addNum;
-				}, smooth);
-			}
+window.addEventListener('DOMContentLoaded', e => {
+	const animNumbers = () => {
+		const offset = elem => {
+			let rect = elem.getBoundingClientRect(),
+					scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+					scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			return {top: rect.top + scrollTop, left: rect.left + scrollLeft};
 		}
-		if ((pageYOffset > offset(featuresNumbersElems[i]).top - animItemPoint) && pageYOffset < (offset(featuresNumbersElems[i]).top + featuresNumbersElems[i].offsetHeight) && featuresNumbersElems.length - 1 === i) {
+
+		const featuresNumbersElems = document.querySelectorAll('.about__feature-title span');
+		const featuresElem = document.querySelector('.about__features');
+		const featuresNumbers = [];
+		const featuresNumbersNotChanged = [];
+		const animStart = 4;
+		let animItemPoint = window.innerHeight - featuresElem.offsetHeight / animStart;
+		if (featuresElem.offsetHeight > window.innerHeight) animItemPoint = window.innerHeight - window.innerHeight / animStart;
+		if ((pageYOffset > offset(featuresElem).top - animItemPoint) && pageYOffset < (offset(featuresElem).top + featuresElem.offsetHeight)) {
+			for (let i = 0, length = featuresNumbersElems.length; i < length; i++) {
+				for (let y = 0; y < featuresNumbersElems.length; y++) {
+					let value = featuresNumbersElems[y].innerHTML;
+					featuresNumbersNotChanged.push(value);
+					featuresNumbers.push(value.replace(/[\s.,\+]/g, ''));
+				}
+
+				const smooth = 70;
+				const seconds = 1000;
+				if (smooth >= 50 && smooth <= seconds) {
+					let value = 0;
+					const num = featuresNumbers[i];
+					const period = seconds / smooth;
+					const addNum = featuresNumbers[i] / period;
+					const increment = setInterval(() => {
+						if (value <= featuresNumbers[i]) {
+							const numberWithCommas = x => x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+							featuresNumbersElems[i].innerHTML = numberWithCommas(Math.trunc(value)) + '+';
+							Math.trunc(value);
+						} else {
+							featuresNumbersElems[i].innerHTML = featuresNumbersNotChanged[i];
+							clearInterval(increment);
+						}
+						value += addNum;
+					}, smooth);
+				}
+			}
 			window.removeEventListener('scroll', animNumbers);
 		}
 	}
-}
 
-window.addEventListener('scroll', animNumbers);
-animNumbers();
+	window.addEventListener('scroll', animNumbers);
+	animNumbers();
+});
 
 /* Open Carts */
 
